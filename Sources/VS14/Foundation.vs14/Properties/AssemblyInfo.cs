56f8c4d892170ef8e2dcc2f79584aae2b04d6b03
Squared Infinity <jarek@squaredinfinity.com>
2015-11-25 22:24:32 +0000

    missing files added

--- Sources/VS14/Foundation.vs14/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a2626117-c984-4eba-a6cc-b21b7dacac38")]
--- Sources/VS14/VscPackage.vs14/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b88c127b-2d17-4f73-99fb-c2b68ce7431d")]
