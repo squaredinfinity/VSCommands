51ded3fa8fa06887cfe2276d42b2276b71d2336b
Squared Infinity <jarek@squaredinfinity.com>
2015-11-29 14:58:32 +0000

    release build fix

--- Sources/Shared/Internal/AssemblyInfo.shared.cs
+++ Sources/Shared/Internal/AssemblyInfo.shared.cs
@@ -9,13 +9,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("14." + CurrentVersionStrings.CommonCurrentReleaseNumber)]
 [assembly: AssemblyFileVersion("14." + CurrentVersionStrings.CommonCurrentReleaseNumber)]
 
-static class CurrentVersionStrings
-{
-    public const string CommonCurrentReleaseNumber = "0.0.2";
-}
-
 #if SIGN
 #pragma warning disable 1699  // warning CS1699: Use command line option '/keyfile' or appropriate project settings instead of 'AssemblyKeyFile'
 [assembly: AssemblyKeyFile(@"c:\!\StartSSL Class 2 - Jaroslaw Kardas.snk")]
 #pragma warning restore 1699
 #endif
+
+static class CurrentVersionStrings
+{
+    public const string CommonCurrentReleaseNumber = "0.0.2";
+}
